c751efadf86d59ae5d0dc141187fe2635c66d5e7
joseluischepo <carrillovargasjoseluis@gmail.com>
2022-11-28 11:43:04 -0600

    GardenHouse 28 11 2022 11:43

--- proyecto_inge/conex.php
+++ proyecto_inge/conex.php
@@ -3,7 +3,7 @@
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'Criptoiot2022');
 define('DB_NAME', 'garden_house');
  
 /* Attempt to connect to MySQL database */
