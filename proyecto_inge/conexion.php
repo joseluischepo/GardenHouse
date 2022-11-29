da1884c59370dc0054149c6e871107d01c3a149a
joseluischepo <carrillovargasjoseluis@gmail.com>
2022-11-29 09:52:12 -0600

    29 11 2022 9:53

--- proyecto_inge/conexion.php
+++
@@ -1,5 +0,0 @@
-<?php
-//Conexión básica a la base de datos, utiliza, usuario, contraseña (por defecto no tiene) y el nombre de la base de datos
-//Hay que asegurarse que la base de datos esté bien escrita, de lo contrario dará error
-$conexion = new mysqli("localhost","ceo","Criptoiot2022","garden_house");
-?>﻿
