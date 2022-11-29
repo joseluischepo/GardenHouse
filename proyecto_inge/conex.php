cc6a5522c71f6806e3051bc78991f074c373048e
joseluischepo <carrillovargasjoseluis@gmail.com>
2022-11-29 10:06:00 -0600

    29 11 2022 10:05

--- proyecto_inge/conex.php
+++ proyecto_inge/conex.php
@@ -2,8 +2,8 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_USERNAME', 'ceo');
+define('DB_PASSWORD', 'Criptoiot2022');
 define('DB_NAME', 'garden_house');
  
 /* Attempt to connect to MySQL database */
