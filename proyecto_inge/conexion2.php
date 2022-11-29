f2ed8e6ceded7980c07e195c557c77f0ed5ecc79
joseluischepo <carrillovargasjoseluis@gmail.com>
2022-11-29 15:25:30 -0600

    29 11 2022 3:26

---
+++ proyecto_inge/conexion2.php
@@ -0,0 +1,10 @@
+<?php
+
+//conexion a la base de datos
+$dbhost = "localhost";  //host local
+$dbuser = "root";		//usuario de BD
+$dbpass = "";	//contraseña de BD
+$dbname = "garden_house";	//nombre de la BD
+
+
+?>
