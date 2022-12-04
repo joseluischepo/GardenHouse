f719e2718be7a3f9dba1ac30957f8ce8a2c2dcfc
joseluischepo <carrillovargasjoseluis@gmail.com>
2022-12-04 16:37:30 -0600

    04 12 2022 4:38

--- proyecto_inge/conexion2.php
+++ proyecto_inge/conexion2.php
@@ -3,7 +3,7 @@
 //conexion a la base de datos
 $dbhost = "localhost";  //host local
 $dbuser = "root";		//usuario de BD
-$dbpass = "";	//contraseña de BD
+$dbpass = "Criptoiot2022";	//contraseña de BD
 $dbname = "garden_house";	//nombre de la BD
 
 
