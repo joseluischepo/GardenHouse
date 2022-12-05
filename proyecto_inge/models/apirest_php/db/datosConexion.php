55c702c0974da23e8150485f73e18f01a79dc666
joseluischepo <carrillovargasjoseluis@gmail.com>
2022-12-05 15:22:03 -0600

    05 12 2022 3:21

--- proyecto_inge/models/apirest_php/db/datosConexion.php
+++
@@ -1,4 +0,0 @@
-<?php
-    //$db = ['servidor' => 'localhost','usuario' => 'root', 'password' => '','db' => 'articulos_db'];
-    $db = ['servidor' => 'localhost','usuario' => 'root', 'password' => 'Criptoiot2022','db' => 'garden_house'];
-    //$db = ['servidor' => '127.0.0.1:3308','usuario' => 'root', 'password' => '','db' => 'maceta'];
